7f55e10cac423f07f15cbe70c16a5f0f0137ce36
PedroKBrant <pkb@cin.ufpe.br>
2023-07-07 19:31:14 -0300

    Feat save results on csv and save heatmaps

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from albumentations.pytorch import ToTensorV2
 import numpy as np
 import argparse
 from tqdm import tqdm
+import csv
 
 transform_input = A.Compose([
     A.Resize(256, 256, interpolation=cv2.INTER_LINEAR),
@@ -31,7 +32,7 @@ def read_image(img_path):
     img = transform_input(image=img)['image'].unsqueeze(0).cuda()
     return img
 
-def evaluate_img(img_path, model):
+def evaluate_img(img_path, model, csv_path):
     img = read_image(img_path)
     with torch.no_grad():
         p_map = model(img)
@@ -42,13 +43,21 @@ def evaluate_img(img_path, model):
     p_map *= 255
     p_map = p_map.astype(np.uint8)
     c_map = cv2.applyColorMap(p_map, colormap=16)
-    saved_name = img_path.split("/")[-1]
-    cv2.imwrite("./results/{0}".format(saved_name), c_map)
+    saved_name = os.path.splitext(os.path.basename(img_path))[0]
+    cv2.imwrite("./results/{0}.png".format(saved_name), c_map)
     #print("File: {0} Score: {1}".format(saved_name, score))
+
+    # Save data to CSV
+    with open(csv_path, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Image', 'Score'])  # Write header row if needed
+        writer.writerow([saved_name, score])
 
 def main(config):
     discriminator = get_model()
     f_path = config.f_path
+    csv_path = config.csv_path
+
     print("Start assessment..")
     if os.path.isfile(f_path):
         evaluate_img(f_path, discriminator)
@@ -56,12 +65,13 @@ def main(config):
         files_list = os.walk(f_path).__next__()[2]
         for file_name in tqdm(files_list):
             file_path = os.path.join(f_path, file_name)
-            evaluate_img(file_path, discriminator)
+            evaluate_img(file_path, discriminator, csv_path)
     print("Done!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--f_path', type=str, default="./docs/00021_Blurred.png", help='file path or folder path')
+    parser.add_argument('--f_path', type=str, default="./docs/toy", help='file path or folder path')
+    parser.add_argument('--csv_path', type=str, default="./results/result.csv", help='csv results file name')
     config = parser.parse_args()
     print(config)
     main(config)
