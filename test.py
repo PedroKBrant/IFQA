0c35e111a7fac20e9298cfdf36ef0fcb938e97a0
PedroKBrant <pkb@cin.ufpe.br>
2023-07-07 21:29:30 -0300

    Feat save original image and heatmap side by side

--- test.py
+++ test.py
@@ -15,6 +15,18 @@ transform_input = A.Compose([
     ToTensorV2()
 ])
 
+def merge_images(img1, img2):
+   # Resize img and c_map to have the same height
+    height = max(img1.shape[0], img2.shape[0])
+    resized_img = cv2.resize(img1, (int(int(img1.shape[1]) * height / int(img1.shape[0])), height), interpolation=cv2.INTER_LINEAR)
+    resized_c_map = cv2.resize(img2, (int(img2.shape[1] * height / img2.shape[0]), height), interpolation=cv2.INTER_NEAREST)
+    width = resized_img.shape[1] + resized_c_map.shape[1]
+    merged_img = np.zeros((height, width, 3), dtype=np.uint8)
+    merged_img[:, :resized_img.shape[1]] = resized_img
+    merged_img[:, resized_img.shape[1]:] = resized_c_map
+
+    return merged_img
+
 def get_model():
     discriminator = Discriminator().cuda()
     model_file_path = "./weights/IFQA_Metric.pth"
@@ -44,7 +56,12 @@ def evaluate_img(img_path, model, csv_path):
     p_map = p_map.astype(np.uint8)
     c_map = cv2.applyColorMap(p_map, colormap=16)
     saved_name = os.path.splitext(os.path.basename(img_path))[0]
-    cv2.imwrite("./results/{0}.png".format(saved_name), c_map)
+
+    img_np = img.squeeze().permute(1, 2, 0).cpu().numpy()
+    img_np = np.clip((img_np * 0.5 + 0.5) * 255, 0, 255).astype(np.uint8)
+    img_np = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    img_map = merge_images(img_np, c_map)
+    cv2.imwrite("./results/{0}.png".format(saved_name), img_map)
     #print("File: {0} Score: {1}".format(saved_name, score))
 
     # Save data to CSV
